648c553940efa334c47cdba93db58fca56ee3abe
dgliang <2433353370@qq.com>
2025-07-29 14:11:25 +0800

    Upgrade LR

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ class Config:
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     BATCH_SIZE = 128
     N_EPOCHS = 400
-    LEARNING_RATE = 0.1
+    LEARNING_RATE = 0.05
     MOMENTUM = 0.9
     WEIGHT_DECAY = 5e-4
     N_CLASSES = 10
     DATA_PATH = 'dataset'
-    CHECKPOINT_PATH = 'checkpoint/resnet18_cifar10.pt'
-    LOG_PATH = 'logs/training.log'
-    TENSORBOARD_PATH = 'runs/resnet18_cifar10'
+    CHECKPOINT_PATH = 'checkpoint/v10.pt'
+    LOG_PATH = 'logs/v10.log'
+    TENSORBOARD_PATH = 'runs/v10'
 
     # 适应 CosineAnnealing 的设置
     COSINE_T_MAX = 100
-    ETA_MIN = 0
+    ETA_MIN = 1e-5
 
     # 适应 CosineAnnealingWarmRestarts 的设置
     T_0 = 10
